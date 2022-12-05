e1f6722f0b1aab1597f771cc634410a0392b62d5
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2022-12-06 00:53:10 +0500

    2_project

---
+++ Zookeeper/zookeeper.py
@@ -0,0 +1,145 @@
+camel = """
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+lion = """
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!"""
+
+deer = """
+Switching on the camera in the deer habitat...
+   /|       |\\
+`__\\\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \\
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+goose = """
+Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \\
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \\
+ <_  `     (  <`<            \              `-._\\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?"""
+
+bat = """
+Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\\
+                ~-. /  \_/  \ .-~
+                   V         V
+This bat looks like it's doing fine."""
+
+rabbit = """
+Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\//\/      .- \\
+ Y        /    Y
+ l       I     !
+ ]\      _\    /"\\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!"""
+
+
+animals = [camel, lion, deer, goose, bat, rabbit]
+
+
+enter = input(
+    'Please enter the number of the habitat you would like to view(exit to exit): ')
+
+
+def check_num(enter):
+    while True:
+        if str(enter) == 'exit':
+            print('See you later!')
+            break
+        elif 0 <= int(enter) < 6:
+            print(animals[int(enter)])
+            enter = input(
+                'Please enter the number of the habitat you would like to view(exit to exit): ')
+        else:
+            print('You must choose from 0 to 6')
+            enter = input(
+                'Please enter the number of the habitat you would like to view(exit to exit): ')
+
+
+check_num(enter)
